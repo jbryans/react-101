55578694a628179f73660e46d5cafd71b4d1f5ad
jbryans <jbryans2014@gmail.com>
2022-07-20 20:49:24 -0500

    second commit

--- src/components/ButtonClass.js
+++ src/components/ButtonClass.js
@@ -2,21 +2,8 @@ import React from "react";
 
 
 
-// export function Button(props) {
-//     console.log(props)
-//     const { label = "Default" } = props;
-//     return <button onClick={() => console.log()}>{props.label}</button>
-// }
-export class ButtonClass extends React.Component {
-    constructor (props) {
-        super(props)
-    }
-
-
-
-render() {
-    const { label = "Default", handleClick = () => console.log("i am default")  } = this.props;
-    // const {handleClick, label} = this.props
-    return <button conClick={handleClick}>{label}</button>
-}
+export function ButtonClass(props) {
+  
+    const { label = "Default", handleClick = () => console.log("i am default") } = props;
+    return <button onClick={handleClick}>{label}</button>
 }
